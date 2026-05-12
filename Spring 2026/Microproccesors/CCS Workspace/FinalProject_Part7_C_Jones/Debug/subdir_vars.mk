c3b4364b79007a9294ff148e4ea51c70e42d2490
Lucas Jones <jones.lr05@gmail.com>
2026-05-11 19:59:02 -0600

    added the final coursework from the spring 2026 semester

---
+++ Spring 2026/Microproccesors/CCS Workspace/FinalProject_Part7_C_Jones/Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr2355.cmd 
+
+C_SRCS += \
+../FinalProject_Part7main.c 
+
+C_DEPS += \
+./FinalProject_Part7main.d 
+
+OBJS += \
+./FinalProject_Part7main.obj 
+
+OBJS__QUOTED += \
+"FinalProject_Part7main.obj" 
+
+C_DEPS__QUOTED += \
+"FinalProject_Part7main.d" 
+
+C_SRCS__QUOTED += \
+"../FinalProject_Part7main.c" 
+
+
