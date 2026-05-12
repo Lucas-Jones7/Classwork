7766d722f812102bbc7677d158829d417587513a
Lucas Jones <jones.lr05@gmail.com>
2026-05-11 20:43:34 -0600

    added remaining classwork from spring 2026 semester

--- Data Mining/Quizzez/Q5/Q5.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-
-G = nx.read_edgelist("aves-thornbill-farine.edges", create_using=nx.Graph(), data=False)
-
-largest_cc = max(nx.connected_components(G), key=len)
-LCC = G.subgraph(largest_cc).copy()
-
-print(nx.average_shortest_path_length(LCC))
